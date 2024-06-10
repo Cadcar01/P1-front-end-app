091526f457b488b60e90ff11b434f21a78927434
anrichter <anrichter2@gmail.com>
2024-06-09 22:20:33 -0500

    fixed upi handleGetQuestions function so that now the correct answer gets randomely spliced in with the possible answers for the quiz to display. Also added more to the renderQuestions function.

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,16 +8,26 @@ function handleGetQuestions() {
     })
 
     .then (function(questions) {
-      console.log(questions)
+      console.log(questions) // DELETE when done
       const quizQuestions = []
 
+      const allPossibleAnswers = []
+
       for (let i = 0; i < questions.results.length; i++) {
-        const individualQuestion = 
-          {type: questions.results[i].type,
-            question: questions.results[i].question,
-            correct_answer: questions.results[i].correct_answer,
-            incorrect_answers: questions.results[i].incorrect_answers
-          }
+        let wrongAnswers = questions.results[i].incorrect_answers;
+        let randomNum = Math.floor(Math.random()*3);
+        wrongAnswers.splice(randomNum, 0, questions.results[i].correct_answer);
+        allPossibleAnswers.push(wrongAnswers);
+      };
+      console.log(allPossibleAnswers); //DELETE when done
+
+      for (let i = 0; i < questions.results.length; i++) {
+        const individualQuestion = {
+          type: questions.results[i].type,
+          question: questions.results[i].question,
+          correct_answer: questions.results[i].correct_answer,
+          all_answers: allPossibleAnswers[i]
+        };
           quizQuestions.push(individualQuestion)
 
       }
@@ -26,40 +36,80 @@ function handleGetQuestions() {
     });
   
 };
+
 handleGetQuestions()
+
 function renderQuestions(quizQuestions) {
   console.log(quizQuestions)
+
+  const questionForm = document.createElement('form')
+  //htmlElement.appendChild(questionForm)
+
   for (let i = 0; i < quizQuestions.length; i++) {
-  questionDiv = document.createElement('div');
-  questionEl = document.createElement('p')
-  questionEl.textContent = quizQuestions[i].question
-  questionDiv.appendChild(questionEl)
+    questionDiv = document.createElement('div');
+    questionEl = document.createElement('p')
+    questionEl.textContent = quizQuestions[i].question
+    //questionForm.appendChild(questionDiv)
+    questionDiv.appendChild(questionEl)
   
+    if (quizQuestions[i].type == 'boolean') {
+      const answerDiv = document.createElement('div');
 
-  if (quizQuestions[i].type == 'boolean') {
-    const answerDiv = document.createElement('div');
-    const trueLabel = document.createElement('label')
-    trueLabel.textContent = 'True'
-    const trueEl = document.createElement('input');
-    trueEl.setAttribute('type', 'checkbox');
-    const falseLabel = document.createElement('label')
-    falseLabel.textContent = 'False'
-    const falseEl = document.createElement('input');
-    trueEl.setAttribute('type', 'checkbox');
-    answerDiv.appendChild(trueLabel, trueEl, falseLabel, falseEl)
+      const trueLabel = document.createElement('label');
+      trueLabel.textContent = 'True'
+      trueLabel.setAttribute('for', `questionTrue${[i]}`);
+      const trueEl = document.createElement('input');
+      trueEl.setAttribute('type', 'checkbox');
+      trueEl.setAttribute('value', 'true');
 
-  } else if (quizQuestions[i].type == 'multiple') {
-    const answerDiv = document.createElement('div');
-    const answerA = document.createElement('input');
-    answerA.setAttribute('type', 'checkbox');
-    const answerB = document.createElement('input');
-    answerB.setAttribute('type', 'checkbox');
-    const answerC = document.createElement('input');
-    answerC.setAttribute('type', 'checkbox');
-    const answerD = document.createElement('input');
-    answerD.setAttribute('type', 'checkbox');
-  }
-  document.body.appendChild(questionDiv)
-}
+      const falseLabel = document.createElement('label')
+      falseLabel.textContent = 'False'
+      trueLabel.setAttribute('for', `questionFalse${[i]}`);
+      const falseEl = document.createElement('input');
+      trueEl.setAttribute('type', 'checkbox');
 
-}
+      answerDiv.appendChild(trueLabel, trueEl, falseLabel, falseEl)
+
+    } else if (quizQuestions[i].type == 'multiple') {
+      const answerDiv = document.createElement('div');
+
+      const labelA = document.createElement('label');
+      labelA.textContent = quizQuestions[i].all_answer[0];
+      const answerA = document.createElement('input');
+      answerA.setAttribute('type', 'checkbox');
+      answerDiv.appendChild(labelA, answerA)
+
+      const labelB = document.createElement('label');
+      labelB.textContent = quizQuestions[i].all_answer[1];
+      const answerB = document.createElement('input');
+      answerB.setAttribute('type', 'checkbox');
+      answerDiv.appendChild(labelB, answerB)
+
+      const labelC = document.createElement('label');
+      labelC.textContent = quizQuestions[i].all_answer[2];
+      const answerC = document.createElement('input');
+      answerC.setAttribute('type', 'checkbox');
+      answerDiv.appendChild(labelC, answerC)
+
+      const labelD = document.createElement('label');
+      labelD.textContent = quizQuestions[i].all_answer[3];
+      const answerD = document.createElement('input');
+      answerD.setAttribute('type', 'checkbox');
+      answerDiv.appendChild(labelD, answerD)
+    }
+    document.body.appendChild(questionDiv)
+  };
+
+  //create button for submitting all the questions
+
+};
+
+//buttonSelector.addEventListener('submit', handleGetQuestions) For getting questions
+
+//selectorOfFormArea.addEventListener('submit', function(event) {
+//event.preventDefault()
+//const = element = event.target
+//if (element.matches('button)) {
+//function to collect data
+// location.assign(second.html)}
+//})
