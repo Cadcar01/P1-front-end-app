2cbe297bd3cf33b2bab9cfe9c1e2e6b3620c6f45
Amanda Lang <Cadenc2003@icloud.com>
2024-06-10 21:29:36 -0500

    added CSS formatting to results page and functionality for back button to return to index.html

---
+++ assets/js/results.js
@@ -0,0 +1,8 @@
+const goBack = document.getElementById('backBtn')
+const catImage = document.getElementById('cataas')
+
+//if (win) {catImage.src='https://cataas.com/cat/cute/says/Good%20Job?font=Impact&fontSize=50&fontColor=green'}
+
+goBack.addEventListener('click', function() {
+    location.href= './index.html'
+})
