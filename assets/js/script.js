6911badf9840131cdabcf4293acf515d5ec21550
anrichter <anrichter2@gmail.com>
2024-06-10 22:17:37 -0500

    Changed checkboxes for the question answers to radio buttons. Added getTriviaURL function so that the user can pick what kind of quiz they want. Added a bunch of attributes for my radio buttons so they work correctly. Added event listeners for the user submitting the model and for the user submitting the answers to the questions.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,53 @@
-baseTriviaURL = 'https://opentdb.com/api.php?amount=10'
-console.log('hit')
+const userChoices = document.getElementById('userchoices');
+const questionForm = document.getElementById('questionform')
 
-function handleGetQuestions() {
-  fetch (baseTriviaURL)
+const baseTriviaURL = 'https://opentdb.com/api.php?amount=10'
+
+function getTriviaURL() {
+
+  if (categoryEl == 'Entertainment: Books') {
+    const userCategory = '&category=10'
+  } else if (categoryEl == 'Entertainment: Film') {
+    const userCategory = '&category=11'
+  } else if (categoryEl == 'Entertainment: Music') {
+    const userCategory = '&category=12'
+  } else if (categoryEl == 'Entertainment: Musicals & Theatres') {
+    const userCategory = '&category=13'
+  } else if (categoryEl == 'Entertainment: Television') {
+    const userCategory = '&category=14'
+  } else if (categoryEl == 'Entertainment: Video Games') {
+    const userCategory = '&category=15'
+  } else if (categoryEl == 'Entertainment: Board Games') {
+    const userCategory = '&category=16'
+  } else if (categoryEl == 'Entertainment: Japanese Anime & Manga') {
+    const userCategory = '&category=31'
+  } else if (categoryEl == 'Entertainment: Cartoon & Animations') {
+    const userCategory = '&category=32'
+  };
+
+  if (difficultyEl == 'Easy') {
+    const userDifficulty = '&difficulty=easy'
+  } else if (difficultyEl == 'Medium') {
+    const userDifficulty = '&difficulty=medium'
+  } else if (difficultyEl == 'Hard') {
+    const userDifficulty = '&difficulty=hard'
+  };
+
+  if (questionType == 'Multiple Choice') {
+    const userQuestionType = '&type=multiple'
+  } else if (questionType == 'True or False') {
+    const userQuestionType = '&type=boolean'
+  } else if (questionType == 'Both') {
+    const userQuestionType = ''
+  };
+
+  const userChosenURL = `https://opentdb.com/api.php?amount=10${userCategory}${userDifficulty}${userQuestionType}`
+  handleGetQuestions(userChosenURL)
+
+};
+
+function handleGetQuestions(userChosenURL) {
+  fetch (userChosenURL)
     .then(function(response) {
       return response.json()
     })
@@ -29,9 +74,9 @@ function handleGetQuestions() {
           all_answers: allPossibleAnswers[i]
         };
           quizQuestions.push(individualQuestion)
-
-      }
-      renderQuestions(quizQuestions)
+      };
+      localStorage.setItem(JSON.stringify('questions', quizQuestions));
+      renderQuestions(quizQuestions);
         
     });
   
@@ -40,76 +85,105 @@ function handleGetQuestions() {
 handleGetQuestions()
 
 function renderQuestions(quizQuestions) {
-  console.log(quizQuestions)
+  console.log(quizQuestions) //DELETE when done
 
-  const questionForm = document.createElement('form')
-  //htmlElement.appendChild(questionForm)
+  const quizForm = document.createElement('form');
+  quizForm.setAttribute('id', 'quiz-form');
+  
+  questionForm.appendChild(quizForm);
 
   for (let i = 0; i < quizQuestions.length; i++) {
     questionDiv = document.createElement('div');
     questionEl = document.createElement('p')
     questionEl.textContent = quizQuestions[i].question
-    //questionForm.appendChild(questionDiv)
+
+    quizForm.appendChild(questionDiv)
     questionDiv.appendChild(questionEl)
   
     if (quizQuestions[i].type == 'boolean') {
-      const answerDiv = document.createElement('div');
+      const answerFieldset = document.createElement('fieldset');
 
       const trueLabel = document.createElement('label');
       trueLabel.textContent = 'True'
-      trueLabel.setAttribute('for', `questionTrue${[i]}`);
+      trueLabel.setAttribute('for', `questionTrue${i}`);
       const trueEl = document.createElement('input');
-      trueEl.setAttribute('type', 'checkbox');
-      trueEl.setAttribute('value', 'true');
+      trueEl.setAttribute('type', 'radio');
+      trueEl.setAttribute('id', `questionTrue${i}`);
+      trueEl.setAttribute('name', `trueOrFalse${i}`);
+      trueEl.setAttribute('value', 'True');
 
       const falseLabel = document.createElement('label')
       falseLabel.textContent = 'False'
-      trueLabel.setAttribute('for', `questionFalse${[i]}`);
+      trueLabel.setAttribute('for', `questionFalse${i}`);
       const falseEl = document.createElement('input');
-      trueEl.setAttribute('type', 'checkbox');
+      trueEl.setAttribute('type', 'radio');
+      trueEl.setAttribute('id', `questionFalse${i}`);
+      trueEl.setAttribute('name', `trueOrFalse${i}`);
+      trueEl.setAttribute('value', 'False');
 
-      answerDiv.appendChild(trueLabel, trueEl, falseLabel, falseEl)
+      answerFieldset.appendChild(trueLabel, trueEl, falseLabel, falseEl);
+      questionDiv.appendChild(answerFieldset);
 
     } else if (quizQuestions[i].type == 'multiple') {
-      const answerDiv = document.createElement('div');
+      const answerFieldset = document.createElement('fieldset');
 
       const labelA = document.createElement('label');
       labelA.textContent = quizQuestions[i].all_answer[0];
+      labelA.setAttribute('for', `questionMulti${i}`);
       const answerA = document.createElement('input');
-      answerA.setAttribute('type', 'checkbox');
-      answerDiv.appendChild(labelA, answerA)
+      answerA.setAttribute('type', 'radio');
+      answerA.setAttribute('id', `questionMulti${i}`);
+      answerA.setAttribute('name', `questionMulti${i}`);
+      answerA.setAttribute('value', `${quizQuestions[i].all_answer[0]}`);
 
       const labelB = document.createElement('label');
       labelB.textContent = quizQuestions[i].all_answer[1];
+      labelB.setAttribute('for', `questionMulti${i}`);
       const answerB = document.createElement('input');
-      answerB.setAttribute('type', 'checkbox');
-      answerDiv.appendChild(labelB, answerB)
+      answerB.setAttribute('type', 'radio');
+      answerB.setAttribute('id', `questionMulti${i}`);
+      answerB.setAttribute('name', `questionMulti${i}`);
+      answerB.setAttribute('value', `${quizQuestions[i].all_answer[1]}`);
 
       const labelC = document.createElement('label');
       labelC.textContent = quizQuestions[i].all_answer[2];
+      labelC.setAttribute('for', `questionMulti${i}`);
       const answerC = document.createElement('input');
-      answerC.setAttribute('type', 'checkbox');
-      answerDiv.appendChild(labelC, answerC)
+      answerC.setAttribute('type', 'radio');
+      answerC.setAttribute('id', `questionMulti${i}`);
+      answerC.setAttribute('name', `questionMulti${i}`);
+      answerC.setAttribute('value', `${quizQuestions[i].all_answer[2]}`);
 
       const labelD = document.createElement('label');
       labelD.textContent = quizQuestions[i].all_answer[3];
+      labelD.setAttribute('for', `questionMulti${i}`);
       const answerD = document.createElement('input');
-      answerD.setAttribute('type', 'checkbox');
-      answerDiv.appendChild(labelD, answerD)
+      answerD.setAttribute('type', 'radio');
+      answerD.setAttribute('id', `questionMulti${i}`);
+      answerD.setAttribute('name', `questionMulti${i}`);
+      answerD.setAttribute('value', `${quizQuestions[i].all_answer[3]}`);
+
+      answerFieldset.appendChild(labelA, answerA, labelB, answerB, labelC, answerC, labelD, answerD);
+      questionDiv.appendChild(answerFieldset);
     }
     document.body.appendChild(questionDiv)
   };
 
-  //create button for submitting all the questions
+  const submitButton = document.createElement('button');
+  submitButton.textContent = 'Submit quiz answers';
+  submitButton.setAttribute('type', 'submit');
+  submitButton.setAttribute('form', 'quiz-form');
 
 };
 
-//buttonSelector.addEventListener('submit', handleGetQuestions) For getting questions
+userChoices.addEventListener('submit', getTriviaURL);
 
-//selectorOfFormArea.addEventListener('submit', function(event) {
-//event.preventDefault()
-//const = element = event.target
-//if (element.matches('button)) {
-//function to collect data
-// location.assign(second.html)}
-//})
+questionForm.addEventListener('submit', function(event) {
+  event.preventDefault()
+
+  const element = event.target
+  if (element.matches('button')) {
+  //function to collect data and save to local storage
+  location.assign('./second.html'); //need to fix second.html location
+  }
+});
