f7d180cc27bed9087539b53adc77ae0e95e9ca3a
Amanda Lang <Cadenc2003@icloud.com>
2024-06-11 21:34:28 -0500

    set up answers to pull from localStorage

--- assets/js/results.js
+++ assets/js/results.js
@@ -1,14 +1,10 @@
 const goBack = document.getElementById('backBtn')
 const results = document.getElementById('winLose')
+const userAnswers = localStorage.getItem(JSON.parse('Answers'))
 
-fetch('https://api.humorapi.com/jokes/random?excludes=nsfw,dark&api-key=35b00f4fc5b341b19c62369d73a8fee6')
-    .then(function(response) {
-        console.log(response)
-        return response.json
-    })
-    .then(function(data) {
-        console.log(data)
-    })
+//fetch('https://api.humorapi.com/jokes/random?api-key=35b00f4fc5b341b19c62369d73a8fee6&excludes=nsfw,dark')
+//.then(function(response) {console.log(response)return response.json})
+//.then(function(data) {console.log(data)})
 
 
 //if (win) {results.textContent = 'Congratulations! You Win!'} else {results.textContent = 'Good Game! Brush up on your pop culture knowledge and try again.'}
