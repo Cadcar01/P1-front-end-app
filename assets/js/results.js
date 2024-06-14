5feb797008681204abbd1ffcc4a44ccb4c21ca22
Amanda Lang <Cadenc2003@icloud.com>
2024-06-13 21:34:27 -0500

    added styles to make page look better

--- assets/js/results.js
+++ assets/js/results.js
@@ -1,25 +1,45 @@
 const goBack = document.getElementById('backBtn')
 const results = document.getElementById('winLose')
-//const userAnswers = localStorage.getItem(JSON.parse('Answers'))
-//const quizAnswers = localStorage.getItem(JSON.parse('questions'))
+const userAnswers = JSON.parse(localStorage.getItem('answers'))
+const quizAnswers = JSON.parse(localStorage.getItem('questions'))
 const jokeEl = document.getElementById('joke')
 const displayAnswers = document.getElementById('answersEl')
 
 function showResults() {
     let userScore = 0
-    displayAnswers.textContent = quizAnswers
-    for (let i=0; i < quizAnswers.length; i++) {
-        if (userAnswers[i] === quizAnswers[i].correct_answers) {
+    for (let i= 0; i < quizAnswers.length; i++) {
+        showAnswers(quizAnswers[i])
+        const userAnswer = document.createElement('p')
+        userAnswer.textContent = `your answer: ${decodeHtml(userAnswers[i])}`
+        userAnswer.classList.add('is-size-5')
+        displayAnswers.appendChild(userAnswer)
+
+
+        if (userAnswers[i] === quizAnswers[i].correct_answer) {
+            userAnswer.classList.add('has-text-success')
             userScore++
         } else {
-            userScore--
+            userAnswer.classList.add('has-text-danger')
+        }
+        if (userScore > 4) {
+        results.textContent = 'Congratulations! You Win!'
+        } else {
+        results.textContent = 'Good Game! Brush up on your pop culture knowledge and try again.'
         }
     }
-    if (userScore > 4) {
-        results.textContent = 'Congratulations! You Win!'
-    } else {
-        results.textContent = 'Good Game! Brush up on your pop culture knowledge and try again.'
-    }
+}
+
+function showAnswers(array) {
+    const question = document.createElement('p')
+    question.textContent = decodeHtml(array.question)
+    question.classList.add('is-size-5', 'has-text-weight-bold')
+    
+    const correctAnswer = document.createElement('p')
+    correctAnswer.textContent = `correct answer: ${array.correct_answer}`
+    correctAnswer.classList.add('is-size-5')
+
+    displayAnswers.appendChild(question)
+    displayAnswers.appendChild(correctAnswer)
 }
 
 function getApi() {
@@ -34,3 +54,12 @@ fetch('https://api.humorapi.com/jokes/random?api-key=35b00f4fc5b341b19c62369d73a
 goBack.addEventListener('click', function() {
     location.href= './index.html'
 })
+
+function decodeHtml(html) {
+    var txt = document.createElement("textarea");
+    txt.innerHTML = html;
+    return txt.value;
+  };
+
+showResults()
+getApi()
